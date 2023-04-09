bfc9a21bb498ac04383b9a084537812668561866
Tijn <tijnvdeijnde@outlook.com>
2023-04-09 17:10:54 +0200

    Added Patch and Link model to make sure that the data is correctly fetched from the database

--- lib/providers/models/launch.dart
+++ lib/providers/models/launch.dart
@@ -3,19 +3,19 @@ import 'package:space_x_tracker/providers/models/core.dart';
 import 'package:space_x_tracker/providers/models/crew.dart';
 import 'package:space_x_tracker/providers/models/failure.dart';
 import 'package:space_x_tracker/providers/models/fairing.dart';
+import 'package:space_x_tracker/providers/models/link.dart';
 
 part 'launch.g.dart';
 
 @JsonSerializable()
 class Launch {
   final bool? autoUpdate;
-  final String? article;
   final List<String>? capsules;
   final List<Core>? cores;
   final List<Crew>? crew;
   final DateTime? dateLocal;
   final String? datePrecision;
-  final DateTime? dateUnix;
+  final int? dateUnix;
   final DateTime? dateUtc;
   final String? details;
   final List<Failure>? failures;
@@ -24,23 +24,21 @@ class Launch {
   final String? id;
   final String? launchLibraryId;
   final String? launchpad;
+  final Link? links;
   final String? name;
   final bool? net;
-  final String? patch;
   final List<String>? payloads;
   final String? rocket;
   final List<String>? ships;
-  final DateTime? staticFireDateUnix;
+  final int? staticFireDateUnix;
   final DateTime? staticFireDateUtc;
   final bool? success;
   final bool? tbd;
   final bool? upcoming;
-  final String? webcast;
   final int? window;
 
   Launch({
     required this.autoUpdate,
-    required this.article,
     required this.capsules,
     required this.cores,
     required this.crew,
@@ -55,9 +53,9 @@ class Launch {
     required this.id,
     required this.launchLibraryId,
     required this.launchpad,
+    required this.links,
     required this.name,
     required this.net,
-    required this.patch,
     required this.payloads,
     required this.rocket,
     required this.ships,
@@ -66,7 +64,6 @@ class Launch {
     required this.success,
     required this.tbd,
     required this.upcoming,
-    required this.webcast,
     required this.window,
   });
 
---
+++ lib/providers/models/link.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:space_x_tracker/providers/models/patch.dart';
+
+part 'link.g.dart';
+
+@JsonSerializable()
+class Link {
+  final Patch? patch;
+  final String? webcast;
+  final String? article;
+
+  Link({
+    required this.patch,
+    required this.webcast,
+    required this.article,
+  });
+
+  factory Link.fromJson(Map<String, dynamic> json) => _$LinkFromJson(json);
+}
