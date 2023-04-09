8057528e0d7a9c7dd24ee0247703649fe1a57669
Tijn <tijnvdeijnde@outlook.com>
2023-04-09 17:18:16 +0200

    Made changes to the list view
    * The list view will now be rebuild once the data is available, this is not quite what I want. I want to wait for the data and then start the initial build.
    * Because all the data attributes are nullable now, I had to add a lot of null checks. I still need to refactor it. This was a quick fix to make sure that the data that is retrieved is displayed correctly.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -45,160 +47,195 @@ class _HomeViewState extends State<HomeView> {
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: ListView.separated(
-        itemCount: launches.length,
-        itemBuilder: (BuildContext context, int index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-              side: BorderSide(
-                color: launches[index].success
-                    ? Theme.of(context).colorScheme.success
-                    : Theme.of(context).colorScheme.error,
-                width: 1.5,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.02,
-                  ),
-                  CircleAvatar(
-                    radius: MediaQuery.of(context).size.width * 0.095,
-                    backgroundColor: launches[index].success
-                        ? Theme.of(context).colorScheme.success
-                        : Theme.of(context).colorScheme.error,
-                    child: CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                      radius: MediaQuery.of(context).size.width * 0.090,
-                      child: SizedBox.fromSize(
-                        child: Image.network(
-                          launches[index].patch,
-                          scale: 4.5,
-                        ),
+      body: _launches.isEmpty == true
+          ? const Placeholder()
+          : Consumer<LaunchProvider>(builder: (context, launch, child) {
+              return ListView.separated(
+                itemCount: _launches.length,
+                itemBuilder: (BuildContext context, int index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(
+                        color: _launches[index].success ?? false
+                            ? Theme.of(context).colorScheme.success
+                            : Theme.of(context).colorScheme.error,
+                        width: 1.5,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.02,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.175,
+                            height: MediaQuery.of(context).size.height * 0.09,
+                            child: Image.network(
+                              _launches[index].links?.patch?.small ??
+                                  'https://images2.imgbox.com/94/f2/NN6Ph45r_o.png',
+                              scale: 4.5,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.04,
+                          ),
+                          Column(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      '#${_launches[index].flightNumber}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      _launches[index].name == null
+                                          ? 'test'
+                                          : _launches[index].name!.substring(
+                                              0,
+                                              min(_launches[index].name!.length,
+                                                  10)),
+                                      style: TextStyle(),
+                                    ),
+                                    Text(
+                                      _launches[index].success ?? false
+                                          ? 'SUCCESS'
+                                          : 'FAILED',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              _launches[index].success ?? false
+                                                  ? Theme.of(context)
+                                                      .colorScheme
+                                                      .success
+                                                  : Theme.of(context)
+                                                      .colorScheme
+                                                      .error),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    _launches[index].dateLocal == null
+                                        ? 'No date'
+                                        : DateFormat('MMM d y, h:mm:ss a')
+                                            .format(
+                                                _launches[index].dateLocal!),
+                                    style: const TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.5,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(Icons.person,
+                                                size: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.05),
+                                            Text(_launches[index]
+                                                        .crew
+                                                        ?.isEmpty ==
+                                                    true
+                                                ? '0'
+                                                : _launches[index]
+                                                    .crew!
+                                                    .length
+                                                    .toString()),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.support,
+                                                size: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.05),
+                                            Text(_launches[index]
+                                                        .cores
+                                                        ?.isEmpty ==
+                                                    true
+                                                ? '0'
+                                                : _launches[index]
+                                                    .cores!
+                                                    .length
+                                                    .toString()),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.luggage,
+                                                size: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.05),
+                                            Text(_launches[index]
+                                                        .payloads
+                                                        ?.isEmpty ==
+                                                    true
+                                                ? '0'
+                                                : _launches[index]
+                                                    .payloads!
+                                                    .length
+                                                    .toString()),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.directions_boat,
+                                                size: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.05),
+                                            Text(_launches[index]
+                                                        .ships
+                                                        ?.isEmpty ==
+                                                    true
+                                                ? '0'
+                                                : _launches[index]
+                                                    .ships!
+                                                    .length
+                                                    .toString()),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.04,
-                  ),
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              '#${launches[index].flightNumber}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(launches[index].name),
-                            Text(
-                              launches[index].success ? 'SUCCESS' : 'FAILED',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: launches[index].success
-                                      ? Theme.of(context).colorScheme.success
-                                      : Theme.of(context).colorScheme.error),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            DateFormat('MMM d y, h:mm:ss a')
-                                .format(launches[index].dateLocal),
-                            style: const TextStyle(
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.5,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.person,
-                                        size:
-                                            MediaQuery.of(context).size.width *
-                                                0.05),
-                                    Text(launches[index].crew?.isEmpty == true
-                                        ? '0'
-                                        : launches[index]
-                                            .crew!
-                                            .length
-                                            .toString()),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.support,
-                                        size:
-                                            MediaQuery.of(context).size.width *
-                                                0.05),
-                                    Text(launches[index].cores.isEmpty == true
-                                        ? '0'
-                                        : launches[index]
-                                            .cores
-                                            .length
-                                            .toString()),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.luggage,
-                                        size:
-                                            MediaQuery.of(context).size.width *
-                                                0.05),
-                                    Text(launches[index].payloads?.isEmpty ==
-                                            true
-                                        ? '0'
-                                        : launches[index]
-                                            .payloads!
-                                            .length
-                                            .toString()),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.directions_boat,
-                                        size:
-                                            MediaQuery.of(context).size.width *
-                                                0.05),
-                                    Text(launches[index].ships?.isEmpty == true
-                                        ? '0'
-                                        : launches[index]
-                                            .ships!
-                                            .length
-                                            .toString()),
-                                  ],
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-      ),
+                ),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              );
+            }),
     );
   }
 }
