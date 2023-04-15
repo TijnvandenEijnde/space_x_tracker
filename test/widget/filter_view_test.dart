a61f952652d2b2ae42c38fb69d24a2341eae8d55
Tijn <tijnvdeijnde@outlook.com>
2023-04-15 13:25:32 +0200

    Added additional tests to make sure that the filter_view is covered
    * Added 3 groups (toggling, saving and removing), toggling will check if the filter items change color when tapped. the saving group will check if the filters will be saved and the removing group will check if the filters will be removed.

--- test/widget/filter_view_test.dart
+++ test/widget/filter_view_test.dart
@@ -1,26 +1,23 @@
 import 'dart:convert';
 
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:space_x_tracker/project_theme.dart';
 import 'package:space_x_tracker/providers/launch_provider.dart';
-import 'package:space_x_tracker/providers/models/launch.dart';
 import 'package:space_x_tracker/views/filter_view.dart';
-import 'package:http/http.dart' as http;
 
 import 'card_list_view_test.mocks.dart';
-import 'launch_data.dart';
 
 @GenerateMocks([http.Client])
 void main() {
   final http.Client client = MockClient();
   final theme = ProjectTheme.lightTheme;
-  final Color whiteColor = theme!.colorScheme.onPrimary;
-  final Color blackColor = theme.colorScheme.onPrimaryContainer;
+  final Color onPrimaryColor = theme!.colorScheme.onPrimary;
+  final Color onPrimaryContainerColor = theme.colorScheme.onPrimaryContainer;
   final Color tertiaryColor = theme.colorScheme.tertiary;
 
   Future<void> createWidgetUnderTest(WidgetTester tester) async {
@@ -35,9 +32,9 @@ void main() {
     await tester.pumpWidget(widgetUnderTest);
   }
 
-  group('filter', () {
+  group('toggling', () {
     testWidgets(
-        'it changes the color of the filter item box to tertiary and the text color to white when clicked',
+        'it changes the color of a nonactive filter item box to tertiary and the text color to on primary when tapped',
         (WidgetTester tester) async {
       await createWidgetUnderTest(tester);
 
@@ -50,7 +47,7 @@ void main() {
           failedContainerWidget.decoration as BoxDecoration;
 
       expect(failedContainerDecoration.color, Colors.transparent);
-      expect(failedText.style?.color, blackColor);
+      expect(failedText.style?.color, onPrimaryContainerColor);
 
       await tester.tap(failed);
       await tester.pumpAndSettle();
@@ -66,43 +63,212 @@ void main() {
           refreshedFailedContainerWidget.decoration as BoxDecoration;
 
       expect(refreshedFailedContainerDecoration.color, tertiaryColor);
-      expect(refreshedFailedText.style?.color, whiteColor);
+      expect(refreshedFailedText.style?.color, onPrimaryColor);
     });
 
-    testWidgets('it will save selected filters when clicked on save',
+    testWidgets(
+        'it changes the color of an active filter item box to transparent and the text color to on primary container when tapped',
+        (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues({
+        'filters': jsonEncode({
+          'statuses': ['failed'],
+          'years': ['2006']
+        })
+      });
+
+      await createWidgetUnderTest(tester);
+      await tester.pumpAndSettle();
+
+      Finder failed = find.text('Failed');
+      Text failedText = tester.widget<Text>(failed);
+      Finder failedContainer =
+          find.ancestor(of: failed, matching: find.byType(Container)).first;
+      Container failedContainerWidget = tester.widget(failedContainer);
+      BoxDecoration failedContainerDecoration =
+          failedContainerWidget.decoration as BoxDecoration;
+
+      expect(failedContainerDecoration.color, tertiaryColor);
+      expect(failedText.style?.color, onPrimaryColor);
+
+      await tester.tap(failed);
+      await tester.pumpAndSettle();
+
+      Finder refreshedFailed = find.text('Failed');
+      Text refreshedFailedText = tester.widget<Text>(refreshedFailed);
+      Finder refreshedFailedContainer = find
+          .ancestor(of: refreshedFailed, matching: find.byType(Container))
+          .first;
+      Container refreshedFailedContainerWidget =
+          tester.widget(refreshedFailedContainer);
+      BoxDecoration refreshedFailedContainerDecoration =
+          refreshedFailedContainerWidget.decoration as BoxDecoration;
+
+      expect(refreshedFailedContainerDecoration.color, Colors.transparent);
+      expect(refreshedFailedText.style?.color, onPrimaryContainerColor);
+    });
+
+    testWidgets(
+        'it will toggle the filters that are saved in the sharedpreferences on initial load',
+        (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues({
+        'filters': jsonEncode({
+          'statuses': ['failed'],
+          'years': ['2006']
+        })
+      });
+
+      await createWidgetUnderTest(tester);
+      await tester.pumpAndSettle();
+
+      Finder failed = find.text('Failed');
+      Text failedText = tester.widget<Text>(failed);
+      Finder failedContainer =
+          find.ancestor(of: failed, matching: find.byType(Container)).first;
+      Container failedContainerWidget = tester.widget(failedContainer);
+      BoxDecoration failedContainerDecoration =
+          failedContainerWidget.decoration as BoxDecoration;
+
+      expect(failedContainerDecoration.color, tertiaryColor);
+      expect(failedText.style?.color, onPrimaryColor);
+    });
+
+    testWidgets('it will toggle active filters', (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues({
+        'filters': jsonEncode({
+          'statuses': ['failed'],
+          'years': ['2006']
+        })
+      });
+
+      await createWidgetUnderTest(tester);
+      await tester.pumpAndSettle();
+
+      Finder failed = find.text('Failed');
+      Text failedText = tester.widget<Text>(failed);
+      Finder failedContainer =
+          find.ancestor(of: failed, matching: find.byType(Container)).first;
+      Container failedContainerWidget = tester.widget(failedContainer);
+      BoxDecoration failedContainerDecoration =
+          failedContainerWidget.decoration as BoxDecoration;
+
+      expect(failedContainerDecoration.color, tertiaryColor);
+      expect(failedText.style?.color, onPrimaryColor);
+    });
+  });
+
+  group('saving', () {
+    setUp(() => SharedPreferences.setMockInitialValues({}));
+
+    testWidgets('it will save selected filters when tapped on save',
         (WidgetTester tester) async {
       await createWidgetUnderTest(tester);
 
-      SharedPreferences.setMockInitialValues({});
       final SharedPreferences preferences =
           await SharedPreferences.getInstance();
 
       Finder failed = find.text('Failed');
-
       Finder year = find.text('2006');
-
       Finder apply = find.text('Apply');
 
       await tester.tap(failed);
-      await tester.pump();
-
       await tester.tap(year);
-      await tester.pump();
-
       await tester.tap(apply);
       await tester.pumpAndSettle();
 
       Text failedText = tester.widget<Text>(failed);
-      Text yearText = tester.widget<Text>(failed);
+      Text yearText = tester.widget<Text>(year);
 
-      expect(failedText.style?.color, whiteColor);
-      expect(yearText.style?.color, whiteColor);
+      expect(failedText.style?.color, onPrimaryColor);
+      expect(yearText.style?.color, onPrimaryColor);
       expect(
           preferences.getString('filters'),
           jsonEncode({
             'statuses': ['failed'],
             'years': ['2006']
           }));
+    });
+
+    testWidgets('it can save one type of selected filters when tapped on save',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final SharedPreferences preferences =
+          await SharedPreferences.getInstance();
+
+      Finder year = find.text('2012');
+      Finder apply = find.text('Apply');
+
+      await tester.tap(year);
+      await tester.tap(apply);
+      await tester.pumpAndSettle();
+
+      Text yearText = tester.widget<Text>(year);
+
+      expect(yearText.style?.color, onPrimaryColor);
+      expect(
+          preferences.getString('filters'),
+          jsonEncode({
+            'years': ['2012']
+          }));
+    });
+
+    group('removing', () {
+      setUp(() => SharedPreferences.setMockInitialValues({
+            'filters': jsonEncode({
+              'statuses': ['failed'],
+              'years': ['2006']
+            })
+          }));
+
+      testWidgets('it will remove all the filters by tapping on discard',
+          (WidgetTester tester) async {
+        await createWidgetUnderTest(tester);
+
+        final SharedPreferences preferences =
+            await SharedPreferences.getInstance();
+
+        expect(
+            preferences.getString('filters'),
+            jsonEncode({
+              'statuses': ['failed'],
+              'years': ['2006']
+            }));
+
+        Finder discard = find.text('Discard');
+
+        await tester.tap(discard);
+        await tester.pumpAndSettle();
+
+        expect(preferences.getString('filters'), null);
+      });
+
+      testWidgets(
+          'it will remove all the filters by tapping the active filter items and tapping apply',
+          (WidgetTester tester) async {
+        await createWidgetUnderTest(tester);
+        await tester.pumpAndSettle();
+
+        final SharedPreferences preferences =
+            await SharedPreferences.getInstance();
+
+        expect(
+            preferences.getString('filters'),
+            jsonEncode({
+              'statuses': ['failed'],
+              'years': ['2006']
+            }));
+
+        Finder failed = find.text('Failed');
+        Finder year = find.text('2006');
+        Finder apply = find.text('Apply');
+
+        await tester.tap(failed);
+        await tester.tap(year);
+        await tester.tap(apply);
+        await tester.pumpAndSettle();
+
+        expect(preferences.getString('filters'), null);
+      });
     });
   });
 }
