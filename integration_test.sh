2c94285cdfbf1c0bd4eba7578baff051c34370d8
Tijn <tijnvdeijnde@outlook.com>
2023-04-22 21:43:04 +0200

    Added new rocket_view_test to the integration.sh script

--- integration_test.sh
+++ integration_test.sh
@@ -3,10 +3,13 @@ flutter drive \
   --target=test/widget/card_list_view_test.dart
 flutter drive \
   --driver=test_driver/integration_test_driver.dart \
-  --target=test/widget/filter_view_test.dart
-flutter drive \
-  --driver=test_driver/integration_test_driver.dart \
-  --target=test/widget/home_view_test.dart
-flutter drive \
-  --driver=test_driver/integration_test_driver.dart \
   --target=test/widget/launch_card_test.dart
+flutter drive \
+  --driver=test_driver/integration_test_driver.dart \
+  --target=test/widget/screens/filter_view_test.dart
+flutter drive \
+  --driver=test_driver/integration_test_driver.dart \
+  --target=test/widget/screens/home_view_test.dart
+flutter drive \
+  --driver=test_driver/integration_test_driver.dart \
+  --target=test/widget/screens/rocket_view_test.dart
